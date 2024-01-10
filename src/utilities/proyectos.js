979da94c58391704484bd4fe88844a7e66177489
Erick <erickgp51@gmail.com>
2024-01-09 20:50:15 -0600

    Mi imagen

--- src/utilities/proyectos.js
+++ src/utilities/proyectos.js
@@ -61,7 +61,7 @@ export const PROYECTOS = [
     titulo: "Tienda de muebles",
     descripcion:
       "Página estática de muebles creada con HTML y CSS. Diseñada con entusiasmo para aprender y perfeccionar mis habilidades.",
-    fecha: "Enero 2020",
+    fecha: "Enero 2023",
     link: "https://github.com/FMColdays/Tienda-de-muebles",
     imagen: "/img/proyecto2img.webp",
     tecnologias: [TAGS.HTML, TAGS.CSS],
@@ -70,7 +70,7 @@ export const PROYECTOS = [
     titulo: "Administrador de pacientes",
     descripcion:
       "Desarrollé un administrador  para el seguimiento de pacientes de una veterinaria para mi aprendizaje en React y Vite.",
-    fecha: "Enero 2020",
+    fecha: "Julio 2023",
     link: "https://github.com/FMColdays/citas_react_vite",
     imagen: "/img/proyecto3img.webp",
     tecnologias: [TAGS.REACT, TAGS.Vite],
@@ -79,7 +79,7 @@ export const PROYECTOS = [
     titulo: "Evaluación de trastornos emocionales",
     descripcion:
       "Desarrollé una aplicación en Laravel que implementa diversos tests de trastornos emocionales. Estos tests son llevados a cabo por estudiantes universitarios, con la finalidad de explorar y comprender de manera práctica la detección de estos trastornos.",
-    fecha: "Enero 2020",
+    fecha: "Octubre 2023",
     link: "https://github.com/FMColdays/TrastornoTest",
     imagen: "/img/proyecto1img.webp",
     tecnologias: [TAGS.LARAVEL, TAGS.PHP],
@@ -88,7 +88,7 @@ export const PROYECTOS = [
     titulo: "Salón de eventos",
     descripcion:
       "Creado desde cero con Laravel. Es una aplicación para para rentar salones de eventos los cuales cuentan con diversos paquetes en los cuales puedes incluir servicios extras.",
-    fecha: "Enero 2020",
+    fecha: "Mayo 2023",
     link: "https://github.com/FMColdays/SalonDeEventos",
     imagen: "/img/proyecto4img.webp",
     tecnologias: [TAGS.LARAVEL],
@@ -97,7 +97,7 @@ export const PROYECTOS = [
     titulo: "Cotizador de criptomonedas",
     descripcion:
       "Esta app fue realizada con React y Vite en la cual veremos como tomar 10 criptomonedas y cotizarlas en tiempo real para obtener su precio al momento.",
-    fecha: "Enero 2020",
+    fecha: "Febrero 2023",
     link: "https://github.com/FMColdays/cotizador-de-criptomonedas",
     imagen: "/img/proyecto5img.webp",
     tecnologias: [TAGS.REACT, TAGS.Vite],
@@ -106,7 +106,7 @@ export const PROYECTOS = [
     titulo: "Control de Presupuesto",
     descripcion:
       "Una aplicación de control de presupuesto desarrollada con React y Vite. Esta herramienta te permite gestionar tus finanzas de manera efectiva, con funciones para realizar un seguimiento detallado de ingresos y gastos, establecer presupuestos mensuales y visualizarlo de manera gráfica.",
-    fecha: "Enero 2020",
+    fecha: "Marzo 2023",
     link: "https://github.com/FMColdays/Administrador-de-gastos",
     imagen: "/img/proyecto6img.webp",
     tecnologias: [TAGS.REACT, TAGS.Vite],
